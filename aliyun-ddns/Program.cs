34c9b3d232006dcb9fca09097ad8b9c62d9611bd
sanjusss <sanjusss@qq.com>
2019-01-15 16:16:07 +0800

    添加项目文件。

---
+++ aliyun-ddns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aliyun_ddns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
