39ff42a005e51b59a2509fbe5c28528694436c9c
sanjusss <sanjusss@qq.com>
2019-01-15 17:19:44 +0800

    测试SDK支持

--- aliyun-ddns/Program.cs
+++ aliyun-ddns/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Aliyun.Acs.Alidns.Model.V20150109;
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Profile;
+using Newtonsoft.Json;
+using System;
 
 namespace aliyun_ddns
 {
@@ -6,7 +10,34 @@ namespace aliyun_ddns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var clientProfile = DefaultProfile.GetProfile("cn-hangzhou");
+            DefaultAcsClient client = new DefaultAcsClient(clientProfile);
+
+            long count = 0;
+            long pageNumber = 1;
+            do
+            {
+                DescribeSubDomainRecordsRequest request = new DescribeSubDomainRecordsRequest();
+                request.SubDomain = "";
+                request.PageSize = 1;
+                request.PageNumber = pageNumber;
+                var response = client.GetAcsResponse(request);
+                foreach (var i in response.DomainRecords)
+                {
+                    Console.WriteLine($"Type={ i.Type } VALUE={ i.Value }");
+                    ++count;
+                }
+
+                if (response.TotalCount <= count)
+                {
+                    break;
+                }
+                else
+                {
+                    ++pageNumber;
+                }
+            } while (true);
+            Console.Read();
         }
     }
 }
