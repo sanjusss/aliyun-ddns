cf38300788bfe27b240da51b5e4e5d175c676e04
sanjusss <sanjusss@qq.com>
2019-01-16 18:44:19 +0800

    增加docker支持

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+﻿
+ARG runtime=microsoft/dotnet:3.0-runtime
+ARG version=0
+
+FROM microsoft/dotnet:3.0-sdk AS build
+MAINTAINER sanjusss <sanjusss@qq.com>
+WORKDIR /src
+COPY . /src
+RUN dotnet publish -c Release -o /app --version-suffix=$version ./aliyun-ddns/aliyun-ddns.csproj
+
+FROM microsoft/dotnet:$runtime AS final
+MAINTAINER sanjusss <sanjusss@qq.com>
+WORKDIR /app
+COPY --from=build /app .
+ENTRYPOINT ["dotnet", "aliyun-ddns.dll"]
---
+++ build.ps1
@@ -0,0 +1,9 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$os = If ($isWindows) {"windows"} Else {"linux"}
+if (${env:ARCH} -eq "amd64") {
+  docker build --pull -t local_image:$os-${env:ARCH} --build-arg "runtime=3.0-runtime" --build-arg "version=$env:APPVEYOR_BUILD_NUMBER" .
+} else {
+  docker build --pull -t local_image:$os-${env:ARCH} --build-arg "runtime=3.0-runtime-stretch-slim-$env:ARCH" --build-arg "version=$env:APPVEYOR_BUILD_NUMBER" .
+}
