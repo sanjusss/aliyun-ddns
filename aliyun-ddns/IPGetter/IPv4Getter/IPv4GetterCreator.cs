1dc4f8b5013e09189c0eaf3dad611a598111be3d
sanjusss <sanjusss@qq.com>
2022-04-22 08:52:27 +0800

    fix #62 移除已经限流的ipip.net接口。

--- aliyun-ddns/IPGetter/IPv4Getter/IPv4GetterCreator.cs
+++ aliyun-ddns/IPGetter/IPv4Getter/IPv4GetterCreator.cs
@@ -10,7 +10,7 @@ namespace aliyun_ddns.IPGetter.IPv4Getter
         private static readonly IEnumerable<IIPv4Getter> _definedGetters = new CommonIPv4Getter[]
             {
                 new CommonIPv4Getter("test-ipv6.com接口", "https://ipv4.lookup.test-ipv6.com/ip/", 100),
-                new CommonIPv4Getter("ipip.net接口", "http://myip.ipip.net/"),
+                //new CommonIPv4Getter("ipip.net接口", "http://myip.ipip.net/"),
                 new CommonIPv4Getter("ip-api.com接口", "http://ip-api.com/json/?fields=query", 100),
                 new CommonIPv4Getter("淘宝接口", "http://ip.taobao.com/service/getIpInfo.php?ip=myip"),
                 new CommonIPv4Getter("3322接口", "http://ip.3322.net/"),
